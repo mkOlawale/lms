fc9db7158937d87b926193dd6f9ac9e37a33d892
mkOlawale <abeebolaking1@gmail.com>
2025-11-23 07:47:58 -0800

    hello

--- server/routes/educatorRoutes.js
+++ server/routes/educatorRoutes.js
@@ -2,8 +2,9 @@ import express from "express"
 import { updateRoleEducator } from "../controller/educatorController.js";
 
 
-
+// new routes for educator
 const educatorRouter = express.Router();
+
 
 educatorRouter.get('/update-role', updateRoleEducator)
 
